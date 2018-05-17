3dbf579611b1df2bb64faa9722cdd772e7efa2b2
Mark Whitehouse <mark.whitehouse@sduhsd.net>
2018-05-17 10:36:39 -0700

    added if stmnt

---
+++ cdemo/ifstmnt_err.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 0;
+
+  // if statement ERROR to test is a is equal to 0
+  if (a = 0)
+  {
+    printf("a is 0\n");
+  }
+  else
+  {
+    printf("a is not 0\n");
+  }
+    
+}
