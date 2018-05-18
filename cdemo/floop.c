95817f940f1a45732166e8a492b40eda4119345a
Mark Whitehouse <mark.whitehouse@sduhsd.net>
2018-05-18 09:04:27 -0700

    more additions

---
+++ cdemo/floop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  
+  // usual for loop
+  for (int i = 0; i < 10; i++)
+  {
+    printf("i is equal to %d\n", i);
+  }
+  
+}
---
+++ cdemo/wloop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 0;
+
+  while (a < 5)
+  {
+    printf("a is equal to %d\n", a);
+    a++;
+  }
+  printf("done - a is equal to %d\n", a);
+}
