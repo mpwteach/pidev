32f69599e3f1b3bf2c987dc4ee92778ec3e05c00
Mark Whitehouse <mark.whitehouse@sduhsd.net>
2018-05-18 08:51:22 -0700

    changes

---
+++ cdemo/pointers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  
+  int a;
+  int* ptrtoa;
+
+  ptrtoa = &a;
+
+  a = 5;
+  printf("The value of a is %d\n", a);
+
+  *ptrtoa = 6;
+  printf("The value of a is %d\n", a);
+
+  printf("The value of ptrtoa is %d\n", ptrtoa);
+  printf("It stores the value %d\n", *ptrtoa);
+  printf("The address of a is %d\n", &a);
+}
